b4d270775cdd7b1e854d77c2f35ff28778e9ff1d
clmb00 <paolo.colombo2000@gmail.com>
2022-11-30 15:49:47 +0100

    [add] censured paragraph and length

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 </head>
 <body>
 
+<!-- Gli animali (Animalia Linnaeus, 1758) o metazoi (Metazoa Haeckel, 1874) sono un regno del dominio degli eucarioti, comprendono in totale più di 1 800 000 specie di organismi classificati, presenti sulla Terra dal periodo ediacarano; il numero di specie via via scoperte è in costante crescita, e alcune stime portano fino a 40 volte superiore la numerosità reale[1]; delle 1,5 milioni di specie animali attuali, 900 000 sono appartenenti solo alla classe degli Insetti.[2] -->
+
   <form action="./next_page.php" method="POST">
     <label for="paragraph_id">Inserisci il testo: </label>
     <textarea name="paragraph" id="paragraph_id" cols="50" rows="10"></textarea>
--- next_page.php
+++ next_page.php
@@ -3,6 +3,8 @@
     $paragraph = $_POST['paragraph'];
     $blocked_word = $_POST['blocked_word'];
 
+    $censored_para = str_replace($blocked_word, '***', $paragraph);
+
 ?>
 
 <!DOCTYPE html>
@@ -11,12 +13,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="#" type="image/x-icon">
+  <style>
+    p{
+      padding-inline: 3rem;
+      text-align: justify;
+    }
+    h2 span{
+      font-style: italic;
+    }
+  </style>
   <title>PHP Badwords - Next Page</title>
 </head>
 <body>
 
+  <h2>Il paragrafo originale è:</h2>
   <p><?php echo $paragraph ?></p>
-  <p><?php echo $blocked_word ?></p>
+  <h4>Ed ha una lunghezza di <?php echo strlen($paragraph) . ' caratteri.'?></h4>
+
+  <hr>
+
+  <h2>Il paragrafo senza la parola "<?php echo "<span>$blocked_word</span>"?>" è:</h2>
+  <p><?php echo $censored_para ?></p>
+  <h4>La nuova lunghezza è di <?php echo strlen($censored_para) . ' caratteri.'?></h4>
   
 </body>
 </html>

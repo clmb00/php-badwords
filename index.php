090143b6b1340af26661af8079e1e12a79816b81
clmb00 <paolo.colombo2000@gmail.com>
2022-11-30 15:37:58 +0100

    [add] form and send data

--- index.php
+++ index.php
@@ -3,8 +3,27 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="#" type="image/x-icon">
   <title>PHP Badwords</title>
+  <style>
+    form > *{
+      display: block;
+      margin-bottom: 10px;
+    }
+    label, button{
+      margin-top: 30px;
+    }
+  </style>
+</head>
 <body>
+
+  <form action="./next_page.php" method="POST">
+    <label for="paragraph_id">Inserisci il testo: </label>
+    <textarea name="paragraph" id="paragraph_id" cols="50" rows="10"></textarea>
+    <label for="blocked_word_id">Inserisci la parola da censurare: </label>
+    <input type="text" name="blocked_word" id="blocked_word_id">
+    <button type="submit">Prosegui</button>
+  </form>
   
 </body>
 </html>
--- next_page.php
+++ next_page.php
@@ -1,11 +1,22 @@
+<?php
+
+    $paragraph = $_POST['paragraph'];
+    $blocked_word = $_POST['blocked_word'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="#" type="image/x-icon">
   <title>PHP Badwords - Next Page</title>
 </head>
 <body>
+
+  <p><?php echo $paragraph ?></p>
+  <p><?php echo $blocked_word ?></p>
   
 </body>
 </html>
